9ed812096b7e7f90dd1a27fd5526f6c3b4f92a17
AlexanderKuzneatsov <kuzneatsov@mail.ru>
2023-01-02 16:29:59 +0300

    EbanayaRekersia

--- БинарныйПоиск1/БинарныйПоиск1.cpp
+++ БинарныйПоиск1/БинарныйПоиск1.cpp
@@ -2,16 +2,27 @@
 
 using namespace std;
 
+int Array(int Size);
+
+void SearchKey(int key, int Size, const int *array);
+
 int main()
 {
 	setlocale(LC_ALL, "ru");
 
 	int Size = 0;
 
+	cout << "Введи размер массива" << endl;
 	cin >> Size;
 	cout << endl;
+	Array(Size);
 
-	int *array = new int[Size];
+}
+
+int Array(int Size)
+{
+	int* array = new int[Size];
+
 
 	for (int i = 0; i < Size; i++)
 	{
@@ -23,28 +34,35 @@ int main()
 
 	cout << "Введите пажилое искомое число" << endl;
 	cin >> key;
+
+
+	SearchKey(key,Size,array);
 	
+	
+	return 0;
+
+	delete[] array;	
+}
+
+void SearchKey(int key, int Size, const int* array)
+{
+	if (key > array[Size] && key < array[0])
+	{
+		cout << "неверное число, ты ебанько?" << endl;
+	}
+
 	int snich = 0;
 
 	int left = 0;
-
-	int left1 = 0;
 
 	int right = Size;
 
 	int middle = (Size / 2);
 
-	if (key > Size && key < left1)
-	{
-		cout << "неверное число, ты ебанько?" << endl;
-		snich = 1;
-	}
-	
-
 
 	while (snich == 0)
 	{
-		
+
 
 		if (array[middle] == key)
 		{
@@ -58,8 +76,8 @@ int main()
 			right = (middle - 1);
 
 			middle = ((left + right) / 2);
-			
-			
+
+
 		}
 		else
 		{
@@ -68,9 +86,7 @@ int main()
 			middle = ((left + right) / 2);
 
 		}
-		
-
 
 	}
-
 }
+
